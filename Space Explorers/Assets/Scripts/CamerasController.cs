316e4cad732c60b8848a2d033f77701fa3776b27
TOSHIBA\Ivan <irosales@luckyintelligence.com>
2018-01-09 09:52:16 -0600

    Proyecto base de Unity

---
+++ Space Explorers/Assets/Scripts/CamerasController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerasController : MonoBehaviour {
+
+    public Camera playerCamera;
+    public Camera worldCamera;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            if (playerCamera.enabled == true)
+            {
+                playerCamera.enabled = false;
+                worldCamera.enabled = true;
+            }
+            else
+            {
+                playerCamera.enabled = true;
+                worldCamera.enabled = false;
+            }
+                
+        }
+        
+    }
+}
---
+++ Space Explorers/Assets/Scripts/WorldCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCamera : MonoBehaviour
+{
+    public Transform target;
+
+    public float distance = 50.0f; //distancia de la camara al personaje
+    public float height = 20.0f;
+
+    public float speed = 100.0f;
+
+    Vector3 position;
+
+    private void Awake()
+    {
+
+        Vector3 position = target.position; //obtiene posición del objeto que se queire seguir
+        position += Quaternion.Euler(0.0f, 0.0f, 0.0f) * new Vector3(0.0f, height, -distance);
+
+        transform.position = position;
+        transform.LookAt(target);
+    }
+
+    void Update()
+    {
+        /*Vector3 position = target.position; //obtiene posición del objeto que se queire seguir
+        position += Quaternion.Euler(0.0f, 0.0f, 0.0f) * new Vector3(0.0f, height, -distance);*/
+
+
+        if (Input.GetAxis("Mouse X") > 0)
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, 0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+        else if (Input.GetAxis("Mouse X") < 0)
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, 0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = position;
+            transform.LookAt(target);
+        }
+            
+    }
+}
