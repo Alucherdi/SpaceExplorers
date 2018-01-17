966d765a953b2346325a61eb64d60ac135431cb0
TOSHIBA\Ivan <irosales@luckyintelligence.com>
2018-01-17 11:54:13 -0600

    Reorganización de carpetas, creación de interfaz y wrapper

---
+++ Assets/Personajes/Wrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wrapper : MonoBehaviour
+{
+    private Interfaz[] abilities;
+
+    void Start()
+    {
+        abilities = GetComponents<Interfaz>();
+        abilities[1].Something();
+    }
+
+    void Update()
+    {
+
+    }
+}
