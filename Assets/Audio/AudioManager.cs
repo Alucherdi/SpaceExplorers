6a6f64537d8a1cf705411909dae9964b0c051a9d
TOSHIBA\Ivan <irosales@luckyintelligence.com>
2018-03-26 18:05:01 -0600

    Scripts de Audio

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static AudioManager instance;
+    public AudioSource musicAudioSource;
+    public AudioClip[] musicCollection;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+
+    }
+    public void MenuMusic()
+    {
+        musicAudioSource.clip = musicCollection[0];
+        musicAudioSource.loop = true;
+        musicAudioSource.Play();
+    }
 }
--- Assets/Audio/SFXManager.cs
+++ Assets/Audio/SFXManager.cs
@@ -1,16 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
+public enum SFXType { }
 public class SFXManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public static SFXManager instance;
+    public AudioSource musicAdioSource;
+    private AudioSource sfxAudioSource;
+    public AudioClip[] sfxCollection;
+
+    public AudioMixer gameAudioMixer;
+
+    public Slider volumenSlider;
+    public Slider sfxSlider;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    void Start () {
+        sfxAudioSource = GetComponent<AudioSource>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        gameAudioMixer.SetFloat("MusicVolumen", volumenSlider.value);
+        gameAudioMixer.SetFloat("SFXVolumen", sfxSlider.value);
+
+        if (volumenSlider.value == -20)
+            musicAdioSource.mute = true;
+        else
+            musicAdioSource.mute = false;
+
+        if (sfxSlider.value == -20)
+            sfxAudioSource.mute = true;
+        else
+            sfxAudioSource.mute = false;
 	}
+
+    public void PlaySFX(SFXType sfxType)
+    {
+        switch(sfxType)
+        {
+
+        }
+    }
 }
