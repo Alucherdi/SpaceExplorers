1ceb7ca696d6d79f4f94f2579cdf3d56d54d1c8d
mostume222 <usju5296@gmail.com>
2018-04-05 09:19:59 -0500

    Prueba movimiento online

--- Assets/Cámara/LookCamera.cs
+++ Assets/Cámara/LookCamera.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class LookCamera : MonoBehaviour {
     public Transform target;
 
+	public void Start()
+	{
+		target = GameObject.Find ("LookTarget").transform;
+	}
+
     void Update()
     {
         transform.LookAt(target);//Dirige la vista hacia el target
---
+++ Assets/Personajes/Prefabs/PlayerSetup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerSetup : NetworkBehaviour{
+   
+	[SerializeField]
+	public Behaviour[] componentsToDisable;
+
+	void Start(){
+		// Disable components
+		if(!isLocalPlayer)
+		{
+			for(int i=0; i<componentsToDisable.Length; i++)
+			{
+				componentsToDisable [i].enabled = false;
+			}
+		}
+	}
+
+
+}
